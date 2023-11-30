0b2d4ff776a2d86f72330b99cd2b70ca326703dc
Bach IT <87653572+Bach2k2@users.noreply.github.com>
2023-11-30 11:17:51 +0700

    Done tutoial 3: Admin site to add records to database

--- myapp/novels/admin.py
+++ myapp/novels/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Novel,Category
+from .models import *
 
 admin.site.register(Novel)
 admin.site.register(Category)
+admin.site.register(Chapter)
