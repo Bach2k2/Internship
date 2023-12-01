93465ce615cfb4fdc474bc95d5539fa1ec2b900d
Bach IT <87653572+Bach2k2@users.noreply.github.com>
2023-12-01 10:04:55 +0700

    Done tutorial part7: Customize admin site

--- myapp/novels/models.py
+++ myapp/novels/models.py
@@ -9,12 +9,9 @@ class Chapter(models.Model):
     content =models.CharField(max_length=2000)
     
 class Novel(models.Model):
+    
     title = models.CharField(max_length=200)
     published_date = models.DateTimeField("date published")
     year= models.IntegerField(default=0)
     status= models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,null = True, blank= True)
-    
-
-
-    # votes = models.IntegerField(default=0)
