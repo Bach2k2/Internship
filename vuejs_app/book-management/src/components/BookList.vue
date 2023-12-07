11387c72fdfc85e7061a95c7c16cb77bb3b0d873
Bach IT <87653572+Bach2k2@users.noreply.github.com>
2023-12-07 16:10:03 +0700

    Done: tutorials

---
+++ vuejs_app/book-management/src/components/BookList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Book {
+  title: string
+  author: string
+  year: number
+}
+const books = [
+    {title:'1', author:'haha',year:2002},
+    {title:'2', author:'haha2',year:2002},
+    {title:'3', author:'haha3',year:2002},
+    {title:'4', author:'hello',year:2002}
+]
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="book in books" :key="book.title">
+            {{ book.title }}
+            {{ book.author }}
+            {{ book.year }}
+        </li>
+    </ul>
+</template>
